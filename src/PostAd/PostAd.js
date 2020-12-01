351aaeb72ec60634542a537346fc5eb0efd56b45
rajni <P10484498@capita.co.uk>
2020-12-02 01:07:50 +0530

    added controls to post form

--- src/PostAd/PostAd.js
+++ src/PostAd/PostAd.js
@@ -11,7 +11,14 @@ class PostAd extends Component {
     state = {
         categories: [],
         subCategories: [],
-        selectedCategory: null
+        selectedCategory: undefined,
+        selectedSubCatgory: undefined,
+        adType:undefined,
+        country:undefined,
+        description:'',
+        location:'',
+        price:'',
+        title:''
     }
 
 
@@ -21,21 +28,30 @@ class PostAd extends Component {
         let selectCateogry = this.props.categories.filter(x => x.catId === currentVal)[0];
 
         this.setState({
-            selectCateogry: selectCateogry,
+            selectedCateogry: currentVal,
             subCategories: selectCateogry.subCategory
         });
     }
 
+    subCategorySelected = event => {
+        this.setState({ selectedSubCatgory: event.target.value })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state, 'submit');
+    }
+
     render() {
         return (
-            <div className="category-container ">
-            <div className="cateogry-title">Post Ads</div>
+            <div className="category-container">
+                <div className="cateogry-title">Post Ads</div>
                 <form onSubmit={this.handleSubmit} className="post-form form-container">
                     <div className="table-wrapper">
                         <label className="first-label">
                             Category
                         </label>
-                        <select className="second-label form-select" value={this.state.value} onChange={this.categorySelected}>
+                        <select className="second-label form-select" value={this.state.selectedCateogry} onChange={this.categorySelected}>
                             <option value="" key="-1"></option>
                             {
                                 this.props.categories.map((item, i) => {
@@ -52,7 +68,8 @@ class PostAd extends Component {
                         <label className="first-label">
                             Sub Category
                     </label>
-                        <select className="second-label form-select" value={this.state.value} onChange={this.handleChange}>
+                        <select className="second-label form-select" value={this.state.selectedSubCatgory} onChange={this.subCategorySelected}>
+                            <option value="" key="-1"></option>
                             {
                                 this.state.subCategories.map((item, i) => {
                                     return (
@@ -64,17 +81,57 @@ class PostAd extends Component {
                     </div>
 
                     <div className="table-wrapper">
-                        <label className="first-label">Price</label>
-                        <input className="second-label form-select" type="number"></input>
+                        <label className="first-label">
+                            Ad Type
+                        </label>
+                        <select className="second-label form-select" value={this.state.adType} onChange={(e) => this.setState({ adType: e.currentTarget.value })}>
+                            <option value="" key="-1"></option>
+                            <option value="Sell" key="1">Sell</option>
+                            <option value="Rent" key="2">Rent</option>
+
+                        </select>
+                    </div>
+
+                    <div className="table-wrapper">
+                        <label className="first-label">
+                            Country
+                        </label>
+                        <select className="second-label form-select" value={this.state.country} onChange={(e) => this.setState({ country: e.currentTarget.value })}>
+                            <option value="" key="-1"></option>
+                            <option value="India" key="ind">India</option>
+                            <option value="England" key="eng">England</option>
+                            <option value="Ireland" key="wale">Wales</option>
+
+                        </select>
+                    </div>
+
+                    <div className="table-wrapper">
+                        <label className="first-label">
+                            Location
+                    </label>
+                        <input type="text" className="second-label form-select"
+                            value={this.state.location} onChange={(e) => this.setState({ location: e.currentTarget.value })} />
+
+                    </div>
+
+                    <div className="table-wrapper">
+                        <label className="first-label">Ad Title</label>
+                        <input className="second-label form-select" type="string"
+                            value={this.state.title} onChange={(e) => this.setState({ title: e.currentTarget.value })} />
                     </div>
 
                     <div className="table-wrapper">
                         <label className="first-label">Description</label>
-                        <textarea  className="second-label descr" />
-
+                        <textarea className="second-label descr"
+                            value={this.state.description} onChange={(e) => this.setState({ description: e.currentTarget.value })} />
                     </div>
 
-                    <div><input type="submit" value="Post" /></div>
+                    <div className="table-wrapper">
+                        <label className="first-label">Price</label>
+                        <input className="second-label form-select" type="number" value={this.state.price} onChange={(e) => this.setState({ price: e.currentTarget.value })} />
+                    </div>
+
+                    <div><input type="submit" value="Submit" /></div>
                 </form>
 
             </div>
